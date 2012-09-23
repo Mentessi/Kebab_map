f87385202595adab68f8d1255c91da20f9388eb8
Paul Thomas <el.pablo.thomas@gmail.com>
2012-09-23 13:20:41 +0100

    added ruby version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KebabmapRails::Application.config.secret_token = 'd8fda4813e3d57eb0956613544fb63e688702d7b2e54621d85913cb4a3a9b55bbbbb8dc1d14469974a2e3b0d98b1307d7b9597b667fbbebfc0c4728ca08d7d23'
---
+++ public/js/map.js
@@ -0,0 +1,160 @@
+$(document).ready(function(){
+
+		getLocation();
+
+function getLocation()
+  {
+  	showMap(51.550, 0.616);
+  if (navigator.geolocation)
+
+    {
+    navigator.geolocation.getCurrentPosition(getLatLng);
+    }
+  	
+  }
+
+/*function showLatLong(position)
+  {
+  $("#demo").html("Latitude: " + position.coords.latitude + 
+  "<br />Longitude: " + position.coords.longitude
+  )};
+*/
+
+function getLatLng (position) {
+	var lat = position.coords.latitude
+	var lng = position.coords.longitude
+
+	//showMap(lat,lng);
+	//showMarker(lat,lng);
+	plotPlaces(lat,lng);
+}
+
+
+
+function showMap (lat,lng) {
+	
+	console.log(lat);
+	console.log(lng);
+	
+		var mapDiv = document.getElementById('map');
+		var latLng = new google.maps.LatLng(lat,lng);
+
+		var options = {
+			center: latLng,
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		var map = new google.maps.Map(mapDiv, options);
+	
+};
+
+
+
+
+function showMarker (lat,lng) {
+
+	console.log(lat);
+	console.log(lng);
+	
+		var mapDiv = document.getElementById('map');
+		var latLng = new google.maps.LatLng(lat,lng);
+		
+		var options = {
+			center: latLng,
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		var map = new google.maps.Map(mapDiv, options);
+		
+		var marker = new google.maps.Marker({
+			position: latLng,
+			map: map,
+		})
+
+};
+
+
+function plotPlaces (lat,lng){
+
+	var mapDiv = document.getElementById('map');
+		var latLng = new google.maps.LatLng(lat,lng);
+		
+		var options = {
+			center: latLng,
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}; //options
+
+		var map = new google.maps.Map(mapDiv, options); //new map
+		
+
+		$.getJSON("http://localhost:3000/data/fsadata.json", function (data) {
+			function getPlaceLat(i){
+				if (data.FHRSEstablishment.EstablishmentCollection.EstablishmentDetail[i].Geocode==null){
+				}
+				else {
+					return parseFloat(data.FHRSEstablishment.EstablishmentCollection.EstablishmentDetail[i].Geocode.Latitude);
+				}
+			}
+
+		function getPlaceLng(i){
+				if (data.FHRSEstablishment.EstablishmentCollection.EstablishmentDetail[i].Geocode==null){
+				} 
+				else {
+					return parseFloat(data.FHRSEstablishment.EstablishmentCollection.EstablishmentDetail[i].Geocode.Longitude);
+				}
+			}		
+			
+			
+			//var placeLatitudes = [];
+			//var placeLongitudes = [];
+			var places = [];
+			var position;
+
+			for (var t= 0; t < data.FHRSEstablishment.EstablishmentCollection.EstablishmentDetail.length; t++) {
+				console.log("Latitude=" + getPlaceLat(t) + ", Longitude=" + getPlaceLng(t));
+
+				if (getPlaceLat(t)==null || getPlaceLng(t)==null) {
+					// do nothing cos position is null
+				}
+				else {
+					position = new google.maps.LatLng(getPlaceLat(t),getPlaceLng(t));
+					var marker = new google.maps.Marker({
+					position: position,
+					map: map,
+					})
+					places.push(new google.maps.LatLng(getPlaceLat(t),getPlaceLng(t)));
+				}
+
+
+				
+				//placeLatitudes.push(getPlaceLat(t));
+
+				//placeLongitudes.push(getPlaceLng(t));
+
+			}
+				//console.log(placeLatitudes);
+				//console.log(placeLongitudes);
+				console.log(places);
+			
+
+		});	//getJSON //arrays
+
+
+
+
+
+
+
+		
+
+}//plotPlaces
+
+}); //document ready
+
+
+
+	
+
