03430f446f9274d0f799af5791717e17492cde9a
Paul Thomas <el.pablo.thomas@gmail.com>
2012-09-23 16:15:06 +0100

    now returning 100 results

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -4,7 +4,7 @@ class LocationController < ApplicationController
   def ratings
     long = params[:long]
     lat = params[:lat]
-    @response_xml = RestClient.get "http://ratings.food.gov.uk/enhanced-search/%5E/%5E/DISTANCE/0/%5E/#{long}/#{lat}/1/30/json"
+    @response_xml = RestClient.get "http://ratings.food.gov.uk/enhanced-search/%5E/%5E/DISTANCE/0/%5E/#{long}/#{lat}/1/100/json"
     render :inline => @response_xml
   end
 end
